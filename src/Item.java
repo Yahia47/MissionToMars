9eae4e5b732d96d47954e733d749cac1b8b42579
Yahia Hassan <yehia4747@gmail.com>
2017-10-27 11:02:35 +0200

    Add getters methods to the Item class

--- src/Item.java
+++ src/Item.java
@@ -1,4 +1,13 @@
 public class Item {
     private String name;
     private int weight;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWeight(){
+        return weight;
+    }
 }
+
