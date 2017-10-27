3e317884b7953515e982ae641abc76b691b56f89
Yahia Hassan <yehia4747@gmail.com>
2017-10-27 14:41:14 +0200

    Add constructor to the Item class

--- src/Item.java
+++ src/Item.java
@@ -2,6 +2,12 @@ public class Item {
     private String name;
     private int weight;
 
+    public Item(String name, int weight)
+    {
+        this.name = name;
+        this.weight = weight;
+    }
+
     public String getName() {
         return name;
     }
