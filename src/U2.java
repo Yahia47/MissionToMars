e3046dac6230c24d36e906aae40dd09ae828d97a
Yahia Hassan <yehia4747@gmail.com>
2017-10-27 14:29:01 +0200

    correct some typos in the U2 class implementation

--- src/U2.java
+++ src/U2.java
@@ -2,9 +2,9 @@ import java.util.Random;
 
 public class U2 extends Rocket {
     // I'm sure there is a better way to do it, but I can't figure it out.
-    private int U1CargoLimit = 29000;
-    private double chanceOfLaunchExplosion = 0.04 * (super.getCurrentWeight() / U1CargoLimit);
-    private double chanceOfLandingCrash = 0.08 * (super.getCurrentWeight() / U1CargoLimit);
+    private int U2CargoLimit = 29000;
+    private double chanceOfLaunchExplosion = 0.04 * (super.getCurrentWeight() / U2CargoLimit);
+    private double chanceOfLandingCrash = 0.08 * (super.getCurrentWeight() / U2CargoLimit);
     private double random = new Random().nextDouble();
 
     public U2() {
