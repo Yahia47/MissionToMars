c520c99b3e636dcae4a19de4d350d5586b53bbd0
Yahia Hassan <yehia4747@gmail.com>
2017-10-27 14:28:06 +0200

    Override the launch and land methods in U2 class

--- src/U2.java
+++ src/U2.java
@@ -1,2 +1,25 @@
+import java.util.Random;
+
 public class U2 extends Rocket {
+    // I'm sure there is a better way to do it, but I can't figure it out.
+    private int U1CargoLimit = 29000;
+    private double chanceOfLaunchExplosion = 0.04 * (super.getCurrentWeight() / U1CargoLimit);
+    private double chanceOfLandingCrash = 0.08 * (super.getCurrentWeight() / U1CargoLimit);
+    private double random = new Random().nextDouble();
+
+    public U2() {
+        // I'm sure there is a better way to do it, but I can't figure it out.
+        super(29000);
+    }
+
+    @Override
+    public boolean launch() {
+        return chanceOfLaunchExplosion >= random;
+    }
+
+    @Override
+    public boolean land() {
+        return chanceOfLandingCrash >= random;
+    }
 }
+
